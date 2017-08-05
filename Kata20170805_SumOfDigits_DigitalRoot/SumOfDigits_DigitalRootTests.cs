25c4998a28cf50dfb7495c86cfa3f75ece4f59eb
cash <cash@cashwu.com>
2017-08-05 15:50:27 +0800

    input 7777 test case

--- Kata20170805_SumOfDigits_DigitalRoot/SumOfDigits_DigitalRootTests.cs
+++ Kata20170805_SumOfDigits_DigitalRoot/SumOfDigits_DigitalRootTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Cryptography;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170805_SumOfDigits_DigitalRoot
@@ -19,6 +20,18 @@ namespace Kata20170805_SumOfDigits_DigitalRoot
             AssertDigitalRootShouldBe(2, 11);
         }
 
+        [TestMethod]
+        public void input_55_should_return_1()
+        {
+            AssertDigitalRootShouldBe(1, 55);
+        }
+
+        [TestMethod]
+        public void input_7777_should_return_1()
+        {
+            AssertDigitalRootShouldBe(1, 7777);
+        }
+
         private static void AssertDigitalRootShouldBe(int expected, int num)
         {
             var number = new Number();
@@ -29,9 +42,13 @@ namespace Kata20170805_SumOfDigits_DigitalRoot
 
     class Number
     {
-        public int DigitalRoot(long n)
+        public long DigitalRoot(long n)
         {
-            return n.ToString().ToCharArray().Select(a => int.Parse(a.ToString())).Sum();
+            while (n > 9)
+            {
+                n = n.ToString().ToCharArray().Select(a => int.Parse(a.ToString())).Sum();
+            }
+            return n;
         }
     }
 }
