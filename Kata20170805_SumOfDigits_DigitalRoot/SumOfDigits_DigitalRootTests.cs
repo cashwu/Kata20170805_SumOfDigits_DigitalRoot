ed4bed86170f757b552109c3775d6f9151476757
cash <cash@cashwu.com>
2017-08-05 15:28:28 +0800

    init

--- Kata20170805_SumOfDigits_DigitalRoot/SumOfDigits_DigitalRootTests.cs
+++ Kata20170805_SumOfDigits_DigitalRoot/SumOfDigits_DigitalRootTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170805_SumOfDigits_DigitalRoot
 {
     [TestClass]
-    public class UnitTest1
+    public class SumOfDigits_DigitalRootTests
     {
         [TestMethod]
         public void TestMethod1()
