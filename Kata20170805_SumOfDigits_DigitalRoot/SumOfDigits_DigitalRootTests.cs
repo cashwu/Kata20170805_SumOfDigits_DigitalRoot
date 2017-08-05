acdb5792893bb143052ee3e5057ab6e8989245b1
cash <cash@cashwu.com>
2017-08-05 15:32:17 +0800

    input 1, test case

--- Kata20170805_SumOfDigits_DigitalRoot/SumOfDigits_DigitalRootTests.cs
+++ Kata20170805_SumOfDigits_DigitalRoot/SumOfDigits_DigitalRootTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170805_SumOfDigits_DigitalRoot
     public class SumOfDigits_DigitalRootTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_1_should_return_1()
         {
+            AssertDigitalRootShouldBe(1, 1);
+        }
+
+        private static void AssertDigitalRootShouldBe(int expected, int num)
+        {
+            var number = new Number();
+            var actual = number.DigitalRoot(num);
+            Assert.AreEqual(actual, expected);
+        }
+    }
+
+    class Number
+    {
+        public int DigitalRoot(long n)
+        {
+            return (int) n;
         }
     }
 }
